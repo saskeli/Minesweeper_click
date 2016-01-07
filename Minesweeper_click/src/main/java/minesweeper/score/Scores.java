490639278a273b4c5a4b465e80958948acdd5f23
Saskeli <tuomionpupu@gmail.com>
2016-01-07 23:11:54 +0200

    fixed compatibility issue

--- Minesweeper_click/src/main/java/minesweeper/score/Scores.java
+++ Minesweeper_click/src/main/java/minesweeper/score/Scores.java
@@ -5,20 +5,21 @@ import java.util.*;
 import minesweeper.util.*;
 
 /**
- * Class represenging a set of scores for minesweeper_click.
- * Typically high scores
- * 
+ * Class represenging a set of scores for minesweeper_click. Typically high
+ * scores
+ *
  * @author dongesa
  */
 public class Scores {
+
     /**
      * Map of scores by GameType
      */
     private EnumMap<GameType, Score> scores;
 
     /**
-     * Class represenging a set of scores for minesweeper_click.
-     * Typically high scores
+     * Class represenging a set of scores for minesweeper_click. Typically high
+     * scores
      */
     public Scores() {
         Serializable s = ObjectStorage.retrieveObject("highscores.dat");
@@ -31,21 +32,26 @@ public class Scores {
 
     @Override
     public String toString() {
-        List<String> sb = new ArrayList<>();
+        List<String> strings = new ArrayList<>();
         for (Score s : scores.values()) {
-            sb.add(s.toString());
+            strings.add(s.toString());
         }
-        return String.join("\n\n", sb);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strings.size() - 1; i++) {
+            sb.append(strings.get(i));
+            sb.append("\n\n");
+        }
+        sb.append(strings.get(strings.size() - 1));
+        return sb.toString();
     }
-    
+
     /**
-     * Checks if the given number of actions for the given gametype
-     * constitute a new record
-     * 
+     * Checks if the given number of actions for the given gametype constitute a
+     * new record
+     *
      * @param gameType the type of game cleared
-     * @param actions  the number of actions to clear
-     * @return         true if the given description is better 
-     *                 than the stored record
+     * @param actions the number of actions to clear
+     * @return true if the given description is better than the stored record
      */
     public boolean isNewRecord(GameType gameType, int actions) {
         if (scores.get(gameType) == null) {
@@ -53,13 +59,13 @@ public class Scores {
         }
         return scores.get(gameType).getActions() > actions;
     }
-    
+
     /**
      * Update the record for the given game type
-     * 
+     *
      * @param gameType the type of game cleared
-     * @param actions  the number of actions used to clear
-     * @param name     the alias of the player
+     * @param actions the number of actions used to clear
+     * @param name the alias of the player
      */
     public void setRecord(GameType gameType, int actions, String name) {
         scores.put(gameType, new Score(name, actions, gameType));
